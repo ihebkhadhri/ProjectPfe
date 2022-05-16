1a8f5003e82ba0c04307c592a3d2e00d15acf4bf
eleeswaidi <elee.swaidi@esprit.tn>
2022-05-16 15:12:19 +0100

    configuration des classes ajoutées

--- ProjectPfe/Models/DbPfeDatabaseSettings.cs
+++ ProjectPfe/Models/DbPfeDatabaseSettings.cs
@@ -10,6 +10,13 @@
         public string IntegrationCollectionName { get; set; } = null!;
         public string UserCollectionName { get; set; } = null!;
         public string CategorieCollectionName { get; set; } = null!;
+        public string TableauCollectionName { get; set; } = null!;
+        public string LigneCollectionName { get; set; } = null!;
+        public string ColonneCollectionName { get; set; } = null!;
+        public string TitreCollectionName { get; set; } = null!;
+        public string Sous_TitreCollectionName { get; set; } = null!;
+        public string ParagrapheCollectionName { get; set; } = null!;
+        public string Sous_ParagrapheCollectionName { get; set; } = null!;
     }
 }
 
