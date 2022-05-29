ca2296b984b8c7f160002bf2ee11aea47c73d668
RamiJammeli <60295241+RamiJammeli@users.noreply.github.com>
2022-05-29 22:38:08 +0100

    dossier lib

---
+++ ProjectPfe/Services/libs/GridFsStockTemplate.cs
@@ -0,0 +1,25 @@
+﻿using ConnexionMongo.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+
+namespace ProjectPfe.Services.libs
+
+{
+    public class GridFsStockTemplate
+    {
+        
+
+        public GridFsStockTemplate(
+            IOptions<DbPfeDatabaseSettings> DatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                DatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                DatabaseSettings.Value.DatabaseName);
+
+            var fs = new GridFSBucket(mongoDatabase);
+        }
+    }
+}
